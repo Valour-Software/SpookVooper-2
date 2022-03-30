8437c8dc0fbd984de895df515fdf4bc38d48e2c6
Jacob <jacoblower4@gmail.com>
2022-03-30 16:08:15 -0400

    added DB and DBCache

---
+++ Database/DBCache.cs
@@ -0,0 +1,110 @@
+using SV2.Database.Models.Entities;
+using SV2.Database.Models.Users;
+using SV2.Database.Models.Groups;
+using SV2.Database.Models.Economy;
+using System.Collections.Concurrent;
+
+
+namespace SV2.Database;
+
+public static class DBCache
+{
+    /// <summary>
+    /// The high level cache object which contains the lower level caches
+    /// </summary>
+    public static Dictionary<Type, ConcurrentDictionary<string, object>> HCache = new();
+
+    public static List<T>? GetAll<T>() where T : class
+    {   
+        var type = typeof(T);
+
+        if (!HCache.ContainsKey(typeof(T)))
+        {
+            return null;
+        }
+        List<T> list = new();
+        foreach (T item in HCache[type].Values) {
+            list.Add(item);
+        }
+        return list;
+    }
+
+    /// <summary>
+    /// Returns true if the cache contains the item
+    /// </summary>
+    public static bool Contains<T>(string Id) where T : class
+    {
+        var type = typeof(T);
+
+        if (!HCache.ContainsKey(typeof(T)))
+            return false;
+
+        return HCache[type].ContainsKey(Id);
+    }
+
+    /// <summary>
+    /// Places an item into the cache
+    /// </summary>
+    public static async Task Put<T>(string Id, T? obj) where T : class
+    {
+        // Empty object is ignored
+        if (obj == null)
+            return;
+
+        // Get the type of the item
+        var type = typeof(T);
+
+        // If there isn't a cache for this type, create one
+        if (!HCache.ContainsKey(type))
+            HCache.Add(type, new ConcurrentDictionary<string, object>());
+
+        if (!HCache[type].ContainsKey(Id)) {
+            HCache[type][Id] = obj;
+        }
+    }
+
+    /// <summary>
+    /// Returns the item for the given id, or null if it does not exist
+    /// </summary>
+    public static T? Get<T>(string Id) where T : class
+    {
+        var type = typeof(T);
+
+        if (HCache.ContainsKey(type))
+            if (HCache[type].ContainsKey(Id)) 
+                return HCache[type][Id] as T;
+
+        return null;
+    }
+
+    public static async Task<IEntity?> FindEntityAsync(string Id)
+    {
+        switch (Id.Substring(0, 1))
+        {
+            case "g":
+                if (Contains<Group>(Id)) {
+                    return Get<Group>(Id);
+                }
+                Group group = await VooperDB.Instance.Groups.FindAsync(Id);
+                await Put<Group>(Id, group);
+                return group;
+            case "u":
+                if (Contains<User>(Id)) {
+                    return Get<User>(Id);
+                }
+                User user = await VooperDB.Instance.Users.FindAsync(Id);
+                await Put<User>(Id, user);;
+                return user;
+            default:
+                return null;
+        }
+    }
+
+    public static async Task SaveAsync()
+    {
+        VooperDB.Instance.Groups.UpdateRange(HCache[typeof(Group)].Values as ICollection<Group>);
+        VooperDB.Instance.Users.UpdateRange(HCache[typeof(User)].Values as ICollection<User>);
+        VooperDB.Instance.TaxPolicies.UpdateRange(HCache[typeof(TaxPolicy)].Values as ICollection<TaxPolicy>);
+        await VooperDB.Instance.SaveChangesAsync();
+    }
+}
---
+++ Database/VooperDB.cs
@@ -0,0 +1,74 @@
+using Microsoft.EntityFrameworkCore;
+using SV2.Database.Models.Users;
+using SV2.Database.Models.Groups;
+using SV2.Database.Models.Economy;
+
+/*  Valour - A free and secure chat client
+ *  Copyright (C) 2021 Vooper Media LLC
+ *  This program is subject to the GNU Affero General Public license
+ *  A copy of the license should be included - if not, see <http://www.gnu.org/licenses/>
+ */
+
+namespace SV2.Database;
+
+public class VooperDB : DbContext
+{
+
+    public static VooperDB Instance = new VooperDB(DBOptions);
+
+    public static string ConnectionString = $"Host={DBConfig.instance.Host};Database={DBConfig.instance.Database};Username={DBConfig.instance.Username};Pwd={DBConfig.instance.Password}";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseNpgsql(ConnectionString, options => { 
+            options.EnableRetryOnFailure(); 
+        });
+        options.UseLowerCaseNamingConvention();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        //modelBuilder.HasCharSet(CharSet.Utf8Mb4);
+    }
+
+    /// <summary>
+    /// This is only here to fulfill the need of the constructor.
+    /// It does literally nothing at all.
+    /// </summary>
+    public static DbContextOptions DBOptions = new DbContextOptionsBuilder().UseNpgsql(ConnectionString, options => {
+        options.EnableRetryOnFailure();
+    }).Options;
+
+
+    /// <summary>
+    /// Table for SV2 users
+    /// </summary>
+    public DbSet<User> Users { get; set; }
+
+    /// <summary>
+    /// Table for SV2 groups
+    /// </summary>
+    public DbSet<Group> Groups { get; set; }
+
+    public DbSet<TaxPolicy> TaxPolicies { get; set; }
+
+    public static async Task<List<TaxPolicy>> GetTaxPoliciesAsync()
+    {
+        List<TaxPolicy>? policies = DBCache.GetAll<TaxPolicy>();
+        // if null, then add all policies from database into the cache
+        if (policies is null) {
+            policies = await VooperDB.Instance.TaxPolicies.ToListAsync();
+            foreach(TaxPolicy policy in policies) {
+                await DBCache.Put<TaxPolicy>(policy.Id, policy);
+            }
+        }
+        return policies;
+        
+    }
+
+    public VooperDB(DbContextOptions options)
+    {
+            
+    }
+}
