f47f692412a686d0bef340006d226e2b55fe42f8
Jacob <jacoblower4@gmail.com>
2022-04-15 19:05:20 -0400

    added basic resource manager that also loads recipes

---
+++ Managers/ResourceManager.cs
@@ -0,0 +1,67 @@
+using System.Threading.Tasks;
+using SV2.Database.Models.Groups;
+using SV2.Database.Models.Economy;
+using SV2.Database.Models.Factories;
+using SV2.Database.Models.Permissions;
+using SV2.Database.Models.Users;
+using SV2.Database.Models.Items;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SV2.Managers;
+
+
+public class Recipe
+{
+    public string Name { get; set; }
+    public Dictionary<string, float> Inputs { get; set; }
+    public Dictionary<string, float> Outputs { get; set; }
+    public float ProcessingCost { get; set; }
+}
+
+public class Material_Group
+{
+    public string Name { get; set; }
+    public List<string> Materials { get; set; }
+}
+
+public class TopLevelResources
+{
+    public List<Material_Group> Material_Groups { get; set; }
+    public List<Recipe> Recipes { get; set; }
+}
+
+public static class ResourceManager 
+{
+    static public List<string> resources = new();
+    static public List<Recipe> Recipes = new();
+    static public List<Material_Group> Material_Groups = new();
+
+    public static async Task Load()
+    {
+        TopLevelResources toplevelresource = await JsonSerializer.DeserializeAsync<TopLevelResources>(File.OpenRead("resources.json"));
+
+        Material_Groups = toplevelresource.Material_Groups;
+
+        resources = toplevelresource.Material_Groups.SelectMany(x => x.Materials).ToList();
+
+        Recipes = toplevelresource.Recipes;
+
+        // need to create item definitions
+
+        foreach(string Resource in resources)
+        {
+            TradeItemDefinition? def = DBCache.GetAll<TradeItemDefinition>().FirstOrDefault(x => x.OwnerId == "g-vooperia" && x.Name == Resource);
+
+            if (def is null) {
+                // now we need to create a definition for this resource
+                def = new TradeItemDefinition("g-vooperia", Resource);
+
+                await DBCache.Put<TradeItemDefinition>(def.Id, def);
+                await VooperDB.Instance.TradeItemDefinitions.AddAsync(def);
+            }
+        }
+        await VooperDB.Instance.SaveChangesAsync();
+    }
+}
