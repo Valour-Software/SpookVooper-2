7c7fa810a125509283aac6d427056aca977abd8b
superjacobl <jacoblower4@gmail.com>
2023-05-17 16:23:53 -0400

    massive rename for blazor

--- SpookVooper-2/Program.cs
+++ SpookVooper-2/Program.cs
@@ -159,6 +159,13 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseWebAssemblyDebugging();
+}
+
+app.UseBlazorFrameworkFiles();
+app.MapFallbackToFile("index.html");
 
 //BaseAPI       .AddRoutes(app);
 ItemAPI        .AddRoutes(app);
